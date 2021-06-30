a8bef302278e32f6fcdedc4996a07a0fe5f7d741
Leonardo Omaña <leotangram@gmail.com>
2021-06-30 16:09:57 -0500

    MovieDB Typed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 import movieDB from '../api/movieDB'
+import { IMovieDBNowPlaying } from '../interfaces/movieInterface'
 
 const HomeScreen = () => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(resp => {
-      console.log(resp.data)
+    movieDB.get<IMovieDBNowPlaying>('/now_playing').then(resp => {
+      console.log(resp.data.results[0].title)
     })
   }, [])
 
